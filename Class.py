b91e42b9bb5a63f52a218dc7f6f401a8046e92b5
MD. SHARIF UDDIN <101212925+sharif4u@users.noreply.github.com>
2024-01-25 19:02:25 +0600

    Update Class.py
    add innstance of variable

--- Class.py
+++ Class.py
@@ -12,3 +12,12 @@ house2 = house(5678, "texas", True , 150000)
 
 print(house1.state)
 print(house2.price)
+
+#Another example of a class variable is the name of the company that builds the houses:
+class flat():
+  compny_name = "Dhaka Builders"
+  def __init__(self, address,state, alarm, price):
+    self.address = address
+    self.state = state
+    self.alarm = alarm
+    self.price = price
